a1044d534103cb8304e8a277ab3d0703d4c903ab
jichangjung@yahoo.com <jichangjung@yahoo.com>
2021-03-22 22:26:18 +0900

    'fix'

--- lib/simple.dart
+++ lib/simple.dart
@@ -82,19 +82,19 @@ void finalExam() {
   print('점을 보세요');
   print('이름을 입력해 주세요');
   stdout.write("Enter your name : ");
-  String name = stdin.readLineSync();
+  String name = stdin.readLineSync()!;
   stdout.write(name);
 
   print('나이를 입력해주세요');
   stdout.write("Enter your age : ");
-  String ageString = stdin.readLineSync();
+  String ageString = stdin.readLineSync()!;
   stdout.write(ageString);
 
   int age = int.parse(ageString);
   var rng = new Random();
   int fortune = rng.nextInt(4) + 1;
   print('점꾀가 나왔습니다!');
-  print('${age}살의 ${name}씨, ')
-
+  print('${age}살의 ${name}씨,/n 당신의 운세 번호는 ${fortune} 입니다');
+  print('1: 대박, 2: 중박, 3: 보통, 4: 망');
 
 }
