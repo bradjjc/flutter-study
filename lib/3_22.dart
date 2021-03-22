89a631b0b17e9b908bf2b20ecf4db1ff3f1bba8a
Jichang Jung <jichangjung@yahoo.com>
2021-03-22 10:26:52 +0900

    first commit

---
+++ lib/3_22.dart
@@ -0,0 +1,37 @@
+void main() {
+  /// 잘못된이름
+  int a = 10;
+  int b = 20;
+
+  /// 잘된이름 (camel case 낙타형)
+  int myAge = 10;
+  int BrotherAge = 20;
+
+  /// snake case 뱀형
+  int my_age = 10;
+  int Brother_age = 20;
+
+  double c = 10.5; // 실수(소수점)
+
+  /// 문자열
+  String name = '홍길동';
+  var name2 = '한석봉';
+
+  /// 진위 (참,거짓)
+  bool isMarried = true; //결혼을함
+  isMarried = false; //결혼을안했다
+
+  /// 숫자 목록
+  List<int> analist = [10, 20, 30];
+  var analist2 = [10, 20, 30];
+
+  /// 문자 목록
+  List<String> nameList = ['홍길동', '한석봉'];
+
+  ///nul 값이없다
+  int g;    /// null///
+  g = 10;   /// null 이아니라 10
+
+  String name3 = '';    /// 아무것도없는 0개의 문자
+  String name4;        /// null
+
