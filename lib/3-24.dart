7188c23256cad815b06980166a476a169541ef1b
Jichang Jung <jichangjung@yahoo.com>
2021-03-24 12:09:31 +0900

    logic

--- lib/3-24.dart
+++ lib/3-24.dart
@@ -1,19 +1,32 @@
-void main (){
+void main() {
+  List<int> scores = [100, 50, 70, 30];
 
-  List<int> scores = [100, 50, 70, 30, 30.5];
+  print(scores.length); // list의 갯수
 
-  int sum = total(scores[0], scores[1], scores[2], scores[3]);
-  double average = avg(scores[0], scores[1], scores[2], scores[3]);
+  int result = 0;
+  for (int i = 0; i < scores.length; i++) {
+    result = result + scores[i];
+  }
+  print(result);
+
+
+  int sum = total(scores);
+  double average = avg(scores);
 
   print('합계 : $sum');
   print('평균 : ${average.toStringAsFixed(2)}'); // 소수점을 끊어주는.toStringAsFixed(2)
 
 }
 
-int total(int korean, int math, int english, int science){
-  return korean + math + english + science;
+
+int total(List<int> scores) {
+  int result = 0;
+  for (int i = 0; i < scores.length; i++) {
+    result = result + scores[i];
+  }
+  return result;
 }
 
-double avg(int korean, int math, int english, int science){
-  return total(korean, math, english, science) / 3;
+double avg(List<int> scores) {
+  return total(scores) / scores.length;
 }
