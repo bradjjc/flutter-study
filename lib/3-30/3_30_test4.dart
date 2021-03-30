f88b9e96d2d5fdcddd357788d78382652b3019c2
bradjjc <jichangjung@yahoo.com>
2021-03-30 12:36:38 +0900

    수정

--- lib/3-30/3_30_test4.dart
+++ lib/3-30/3_30_test4.dart
@@ -1,4 +1,3 @@
-
 // 두 정수를 입력받아 비교하여 더 큰 수를 출력하는 프로그램
 // 두 수가 같은 경우 eq를 출력
 
@@ -14,13 +13,18 @@ void main() {
   Exam exam = Exam();
   print(exam.solution(10, 20));
   print(exam.solution(3, 3));
-
-
 }
+
 class Exam {
-  int solution(int a, int b) {
-    int result = 0;
-    if(solution.reduce(max));
-    return max;
+  String solution(int a, int b) {
+
+
+    if (a > b) {
+      return '$a';
+    } else if (a < b) {
+      return '$b';
+    } else if (a == b) {
+      return 'ed';
+    }
   }
 }
