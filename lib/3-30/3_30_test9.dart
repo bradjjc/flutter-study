caa97f0cee66b774f7970938607bd91c912b5c69
bradjjc <jichangjung@yahoo.com>
2021-03-30 17:59:56 +0900

    9

--- lib/3-30/3_30_test9.dart
+++ lib/3-30/3_30_test9.dart
@@ -1,64 +1,31 @@
-//
-// 다음과 같이 A~Z 까지 알파벳 대문자 중에서 한 문자 s를 입력받습니다.
-// ABCDEFGHIJKLMNOPQRSTUVWXYZ
-//
-// A를 1번째로 하면 C는 3번째입니다.
-// 입력받은 문자가 몇 번째 문자인지 출력하시오
+// -100 부터 100까지 중 정수 n이 주어졌을 때 절대값을 구하시오
 //
 // 입력
 // 출력
 //
-// C
-// 3
+// -10
+// 10
 //
-// X
-// 24
+// -1
+// 1
 //
-
+// 15
+// 15
 void main() {
   Exam exam = Exam();
-  print(exam.solution('C'));
-  print(exam.solution('X'));
+  print(exam.solution(-10));
+  print(exam.solution(-1));
+  print(exam.solution(15));
 }
 
 class Exam {
-  int solution(String eng) {
+  int solution(int number) {
     int answer = 0;
-    List<String> list = [
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F',
-      'G',
-      'H',
-      'I',
-      'J',
-      'K',
-      'L',
-      'M',
-      'N',
-      'O',
-      'P',
-      'Q',
-      'R',
-      'S',
-      'T',
-      'U',
-      'V',
-      'W',
-      'X',
-      'Y',
-      'Z'
-    ];
 
-
-    for (int i = 0; i < list.length; i++) {
-      if (eng == list[i]) {
-        answer = i + 1;
-      }
-
+    if (number < 0) {
+      answer = number * -1;
+    } else if (number > 0){
+      answer = number;
     }
     return answer;
   }
