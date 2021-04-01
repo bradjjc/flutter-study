3abf4240717236fcbf90e2d0d103929a35d9e003
bradjjc <jichangjung@yahoo.com>
2021-04-01 11:28:13 +0900

    fix

--- lib/4_1_test1/4_1_2.dart
+++ lib/4_1_test1/4_1_2.dart
@@ -18,13 +18,13 @@ import 'dart:io';
 //   }
 // }
 
-void main(){
+void main() {
   String s = stdin.readLineSync();
   int result = 0;
   for (int i = 0; i < s.length; i++)
-     if (s[i] == 'A') {
-       result += 1;
-     }
+    if (s[i] == 'A') {
+      result += 1;
+    }
 
   print(result);
 }
