790e68ddb159a21ae1cd3ebaeef1171b8e032bdf
bradjjc <jichangjung@yahoo.com>
2021-04-01 13:49:17 +0900

    fix

--- lib/4_1_test1/4_1_3.dart
+++ lib/4_1_test1/4_1_3.dart
@@ -1,4 +1,3 @@
-
 // 당신은 0 % ~ 100 % 강수 확률 수치를 얻고, 그 수치에서 날씨를 문자열로 표시를 하려고 생각했습니다.
 // 표시 규칙은 다음과 같이 정했습니다.
 // · 강수 확률이 0 % 이상 30 % 이하이면 “sunny”
@@ -15,22 +14,22 @@
 
 import 'dart:io';
 
-void main(){
+void main() {
   Exam exam = Exam();
   int s = int.parse(stdin.readLineSync());
 
   print(exam.solution(s));
-
 }
+
 class Exam {
   String solution(int s) {
-    String result;
-    for (int i = 0; i < 100; i++)
-      if (i >= 30) {
+    String result = '';
+
+      if (s >= 0 && s <= 30) {
         result = 'sunny';
-      } else if (i <= 70) {
+      } else if (s >= 31 && s <= 70) {
         result = 'cloudy';
-      } else if (i > 70) {
+      } else if (s >= 71) {
         result = 'rainy';
       }
     return result;
