25853c1ec0333c202fade45c7ac10dde79e1c531
bradjjc <jichangjung@yahoo.com>
2021-04-01 11:27:35 +0900

    수정

--- lib/4_1_test1/4_1_2.dart
+++ lib/4_1_test1/4_1_2.dart
@@ -1,19 +1,30 @@
 import 'dart:io';
 
-void main() {
-  Exam exam = Exam();
+// void main() {
+//   Exam exam = Exam();
+//   String s = stdin.readLineSync();
+//
+//   print(exam.solution(s));
+// }
+//
+// class Exam {
+//   int solution(String s) {
+//     int result = 0;
+//     for (int i = 0; i < s.length; i++)
+//       if (s[i] == 'A') {
+//         result += 1;
+//       }
+//     return result;
+//   }
+// }
+
+void main(){
   String s = stdin.readLineSync();
+  int result = 0;
+  for (int i = 0; i < s.length; i++)
+     if (s[i] == 'A') {
+       result += 1;
+     }
 
-  print(exam.solution(s));
-}
-
-class Exam {
-  int solution(String s) {
-    int result = 0;
-    for (int i = 0; i < s.length; i++)
-      if (s[i] == 'A') {
-        result += 1;
-      }
-    return result;
-  }
+  print(result);
 }
