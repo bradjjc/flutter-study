c9fb0ee0d670d3313116fa4f5fa64485132dde9e
bradjjc <jichangjung@yahoo.com>
2021-04-03 21:57:21 +0900

    fix

--- lib/4_2/4.2AddImage.dart
+++ lib/4_2/4.2AddImage.dart
@@ -1,16 +1,27 @@
+// import 'dart:io';
+//
+//
+// void main(){
+//   String E = stdin.readLineSync();
+//
+//   int result = 0;
+//   for (int i = 0; i < E.length; i++){
+//     if (E[i] == '<'){
+//       result += 10;
+//     } else if (E[i] == '/'){
+//       result += 1;
+//     }
+//   }
+//   print(result);
+// }
+
 import 'dart:io';
 
-
-void main(){
+void main() {
   String E = stdin.readLineSync();
 
-  int result = 0;
-  for (int i = 0; i < E.length; i++){
-    if (E[i] == '<'){
-      result += 10;
-    } else if (E[i] == '/'){
-      result += 1;
-    }
-  }
-  print(result);
+  int ten = E.length - E.replaceAll('<', '').length;
+  int one = E.length - E.replaceAll('/', '').length;
+
+  print(ten * 10 + one);
 }
--- lib/4_2/4_2_9.dart
+++ lib/4_2/4_2_9.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 void main() {
   List<String> inputLine = stdin.readLineSync().split(' ');
   int H = int.parse(inputLine[0]);
