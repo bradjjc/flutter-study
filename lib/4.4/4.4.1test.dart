79b3470433d06846116b9ca76c179232b5de1c8e
bradjjc <jichangjung@yahoo.com>
2021-04-04 17:21:17 +0900

    test

---
+++ lib/4.4/4.4.1test.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  List<int> input = stdin.readLineSync().split(' ').map((e) => int.parse(e)).toList();
+  int N = input[0];
+  int X = input[1];
+
+  List<int> input2 = stdin.readLineSync().split(' ').map((e) => int.parse(e)).toList();
+  String result = '';
+
+  for (int i = 0; i < N; i++){
+
+    if (input2[i] < X){
+      result += '${input2[i]} ';
+    }
+  }print(result);
+}
