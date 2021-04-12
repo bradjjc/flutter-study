54a9c8b004eb0e77a73c259784c3f1a73450e169
bradjjc <jichangjung@yahoo.com>
2021-04-12 11:02:18 +0900

    self

--- lib/practice/4.11.dart
+++ lib/practice/4.11.dart
@@ -1,20 +1,53 @@
-import 'dart:io';
-
 void main() {
-  List<String> input = stdin.readLineSync().split(' ').toList();
-  int result = 0;
-  for (int i = 0; i < input.length; i++){
-    for (int j = 1; j < input.length; j++){
-    if (input[i] == '1' && input[j] == '3' ){
-      result += 1;
-    }else if (input[i] == '2' && input[j] == '1' ){
-      result += 1;
-    }else if (input[i] == '3' && input[j] == '2' ) {
-      result += 1;
-    }else {
-      result += 0;
-    }
+  String input = stdin.readLineSync();
+  var winners = {}; // 빈 Map
+  for (int i = 0; i < input.length; i++) {
+    for (int j = 0; j < input.length; j++) {
+      if (input[i] == '1' && input[j] == '3') {
+        winners[i] = true;
+        //winners = {
+        //     0: true,
+        //   }
+      } else if (input[i] == '2' && input[j] == '1') {
+        winners[i] = true;
+      } else if (input[i] == '3' && input[j] == '2') {
+        winners[i] = true;
+      }
     }
   }
-  print(result);
+  print(winners.length);
 }
+// import 'dart:io';
+// void main() {
+//   List<int> input =
+//   stdin.readLineSync().split(' ').map((e) => int.parse(e)).toList();
+//   List<Person> people = input.map((e) => Person(e)).toList();
+//   for (int i = 0; i < people.length; i++) {
+//     for (int j = 0; j < people.length; j++) {
+//       people[i].fight(people[j]);
+//     }
+//   }
+//   int result = 0;
+//   for (var person in people) {
+//     if (person.winner) {
+//       result++;
+//     }
+//   }
+//   print(result);
+// }
+// class Person {
+//   int hand; // 1, 2, 3 => 가위, 바위, 보
+//   bool winner = false;
+//   Person(this.hand);
+//   bool fight(Person other) {
+//     bool result = false;
+//     if (hand == 1 && other.hand == 3) {
+//       winner = true;
+//       result = true;
+//     } else if (hand - other.hand == 1) {
+//       winner = true;
+//       result = true;
+//     }
+//     return result;
+//   }
+// }
